ec758a597a78dbc306c54a8a8df9cd050f65105c
Dario Di Gulio <DGDarioGabriel@proveedor.teco.com.ar>
2025-01-24 17:24:47 -0300

    Agrega loggin

--- app/db.py
+++ app/db.py
@@ -1,5 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+import logging
 
 db = SQLAlchemy()
 migrate = Migrate()
@@ -7,3 +8,13 @@ migrate = Migrate()
 def init_app(app):
     db.init_app(app)
     migrate.init_app(app, db)
+
+    with app.app_context():
+        try:
+            # Prueba de conexión a la base de datos
+            result = db.session.execute("SELECT 1").fetchone()
+            logging.info("✅ Conexión exitosa a la base de datos.")
+            print("✅ Conexión exitosa a la base de datos.")
+        except Exception as e:
+            logging.error(f"❌ Error al conectar a la base de datos: {e}")
+            print(f"❌ Error al conectar a la base de datos: {e}")
